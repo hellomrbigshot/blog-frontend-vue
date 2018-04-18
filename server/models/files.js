dff7fb11b66bcd36804d13e3e6f000b77ce6243e
hellomrbigshot <1994yechen@gmail.com>
2018-04-18 17:37:42 +0800

    update

--- server/models/files.js
+++ server/models/files.js
@@ -1,15 +1,31 @@
 const File = require('../lib/file').File
+const path = require('path')
+const fs = require('fs')
 
-module.exports = {
-	// 注册一个用户
+const fileOperate = {
+	// 上传一个文件
 	create (file) {
 		return File.create(file)
 	},
-	// 通过用户名获取用户信息
+	// 通过 id 获取文件
 	getFileById (id) {
 		return File
 			.findOne({ _id: id })
 			// .addCreatedAt()
 			.exec()
+	},
+	// 通过 id 删除文件
+	async deleteFileById (id) {
+		let fileObject = await fileOperate.getFileById(id)
+		fs.unlink(path.join('../uploads',fileObject.filename))
+		return File
+			.remove({ _id: id })
+			.exec()
+	},
+	// 获取文件路径
+	getFilePath (id) {
+		let fileObject = await fileOperate.getFileById(id)
+		return path.join('../uploads',fileObject.filename)
 	}
 }
+module.exports = fileOperate
