73c3d941e8847d12e17fca9913969dbe3e80a6c2
hellomrbigshot <1994yechen@gmail.com>
2018-05-07 00:57:01 +0800

    update

--- src/vuex/modules/user.js
+++ src/vuex/modules/user.js
@@ -10,8 +10,11 @@ const user = {
     },
     mutations: {
         updatePageNum (state, obj) {
-            state.page_num = obj.page_num
-            state.draft_num = obj.draft_num
+            console.log(obj)
+            Vue.set(state, 'page_num', obj.page_num)
+            Vue.set(state, 'draft_num', obj.draft_num)
+            // state.page_num = obj.page_num
+            // state.draft_num = obj.draft_num
             if (localStorage.getItem('user')) {
                 let user = JSON.parse(localStorage.getItem('user'))
                 user.page_num = obj.page_num
