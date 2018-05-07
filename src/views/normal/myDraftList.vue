5da7f5939b66a3c0c06bd591bc67696fe005dd71
hellomrbigshot <1994yechen@gmail.com>
2018-05-07 15:17:29 +0800

    update

--- src/views/normal/myDraftList.vue
+++ src/views/normal/myDraftList.vue
@@ -1,22 +1,22 @@
 <template>
 <div>
-    <div v-for="(draft, index) in draft_list">
-        <article>
-            <header>
-                <router-link :to="{name: 'editPage', params: {id: draft._id}}">
-                    <h1 class="page-title">{{ (index + 1) + '.' + draft.title }}</h1>
-                </router-link>
-                <div class="page-info">
-                    <span class="create-time">创建于 <time>{{ draft.create_date.substring(0,10) }}</time></span>
-                    <!-- <span class="create-user">&nbsp;|&nbsp;作者 
-                        <router-link :to="{name: 'userDetail', params :{username:  draft.create_user}}">
-                            <span class="user-span">{{ draft.create_user }}</span>  
-                        </router-link></span>
-                    <span>&nbsp;|&nbsp;</span> -->
-                </div>
-            </header>
-        </article>
+    <div class="topic">
+        <h1>草稿箱</h1>
     </div>
+    <ul class="draft-ul">
+        <li v-for="(draft, index) in draft_list" class="draft-li">
+            <Row>
+                <Col :xs="24" :sm="24" :md="18">
+                    <router-link :to="{name: 'editPage', params: {id: draft._id}}">
+                        <div class="draft-title">{{ draft.title }}</div>
+                    </router-link>
+                </Col>
+                <Col :xs="24" :sm="24" :md="6">
+                    <div class="create-time">更新于 <time>{{ draft.update_date.substring(0,10) }}</time></div>
+                </Col>
+            </Row>
+        </li>
+    </ul>
 </div>
 
 </template>
@@ -40,37 +40,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-article {
-    header {
-        font-family: Lato, "PingFang SC", "Microsoft YaHei", sans-serif;
-        .page-title {
+.topic {
+    h1 {
+        font-weight: 400;
+        font-size: 20px;
+    }
+    padding-bottom: 30px;
+}
+.topic, .draft-li {
+    border-bottom: 1px solid #e9eaec;
+}
+.draft-ul {
+    list-style: none;
+    .draft-li {
+        padding: 10px 0;
+        .draft-title {
+            padding-right: 20px;
             font-weight: 400;
-            font-size: 20px;
-            margin: 20px 0 10px;
+            font-size: 14px;
             width: auto;
-            display: inline-block;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
             color: #000;
-            border-bottom: 2px solid #fff;
             &:hover {
-                border-bottom: 2px solid #000;
+                text-decoration: underline;
             }
         }
-        .page-info {
+        .create-time {
+            padding: 0 10px;
             color: #999;
-            font-size: 12px;
-            margin: 0 0 20px;
-            .user-span {
-                color: #999;
-                display: inline-block;
-                border-bottom: 1px solid #fff;
-                &:hover {
-                    border-bottom: 1px solid #999;
-                }
-            }
         }
     }
-    margin-bottom: 120px;
-    // max-height: 300px;
-    // overflow: hidden;
 }
+
 </style>
