db591f0d1e420f51822a1d4234b57862ca6f3ca6
hellomrbigshot <1994yechen@gmail.com>
2018-06-15 00:49:43 +0800

    create comment schema

---
+++ server/lib/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('./mongo')
+const Schema = mongoose.Schema
+
+const CommentSchema = new Schema({
+    content: { type: String }, // 评论内容
+    createuser: { type: String }, // 创建人
+    pageid: { type: String }, // 关联文章
+    creattime: { type: Date, default: Date.now }, // 创建时间
+
+})
+
+exports.Comment = mongoose.model('Comment', CommentSchema)
--- server/lib/file.js
+++ server/lib/file.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const FileSchema = new Schema({
     originalname: { type: String }, // 原文件名
     filename: { type: String }, // 现文件名
-    updatetime: { type: Date, default: Date.now}  // 更新时间
+    updatetime: { type: Date, default: Date.now }  // 更新时间
 })
 
 exports.File = mongoose.model('File', FileSchema)
