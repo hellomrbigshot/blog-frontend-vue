a3b13189e0337331008243bbb80189801f5b8edd
hellomrbigshot <1994yechen@gmail.com>
2018-09-05 11:37:23 +0800

    add loading

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,33 +1,66 @@
 import axios from 'axios'
+import { Message } from 'iview'
 import qs from 'qs'
 import {router} from '@/router/index'
 import Cookies from 'js-cookie'
-import { Message } from 'iview'
+
+let timer = null;
+let loadingArr = [];
+// 添加 loading
+axios.interceptors.request.use(config => {
+    if (!loadingArr.length) {
+        timer = setTimeout(() => {  // 添加延时函数，如果接口请求很快就不出现 loading
+            Message.loading({
+                content: 'Loading...',
+                duration: 0
+            })
+        }, 300)
+    }
+    loadingArr.length ++ 
+    return config
+},
+error => {
+    console.log(error)
+    return Promise.reject(error)
+})
 
 // axios 拦截器 未登录则跳转到登录页
-axios.interceptors.response.use(
-    res => {
-        if (res.data.code === 'OK') {
-            return res
-        } else {
-            Message.error(res.data.data)
-            return Promise.reject(res.data)
-        }    
-    },
-    error => {
-        if (error.response) {
-            switch (error.response.status) {
-                case 401:
-                    Cookies.remove('user')
-                    localStorage.removeItem('user')
-                    router.replace({
-                        name: 'login',
-                        query: { redirect: router.currentRoute.fullPath }
-                    })
-            }
+axios.interceptors.response.use(res => {
+    loadingArr.pop()
+    if (timer) {
+        clearTimeout(timer)
+        timer = null
+    }
+    if (!loadingArr.length) {
+        if (Message) Message.destroy()    
+    }
+
+    if (res.data.code === 'OK') {
+        return res
+    } else {
+        Message.error(res.data.data)
+        return Promise.reject(res.data)
+    }    
+},
+error => {
+    loadingArr.pop()
+    if (!loadingArr.length) {
+        if (Message) Message.destroy()    
+    }
+    if (error.response) {
+        switch (error.response.status) {
+            case 401:
+                Cookies.remove('user')
+                localStorage.removeItem('user')
+                router.replace({
+                    name: 'login',
+                    query: { redirect: router.currentRoute.fullPath }
+                })
         }
-        return Promise.reject(error.response.data)
-    })
+    }
+    console.log(error)
+    return Promise.reject(error.response.data)
+})
 
 export const Common = {
     axios (url, params) {
