f0b9f2c3caaf3d896569665cd2699d7cbfacff90
hellomrbigshot <1994yechen@gmail.com>
2019-01-30 18:22:10 +0800

    update checkbox

---
+++ src/components/mUi/src/components/checkbox/checkbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <label>
+    <span>
+      <input 
+        type="checkbox"
+        :disabled="disabled"
+        :checked="currentValue"
+        @change="change">
+    </span>
+    <slot></slot>
+  </label>
+</template>
+<script>
+export default {
+  name: 'mCheckbox',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [String, Number, Boolean],
+      default: false
+    },
+    trueValue: {
+      type: [String, Number, Boolean],
+      default: true
+    },
+    falseValue: {
+      type: [String, Number, Boolean],
+      default: false
+    }
+  },
+  data () {
+    return {
+      currentValue: this.value
+    }
+  },
+  methods: {
+    change (event) {
+      if (this.disabled) return false;
+      const checked = event.target.checked;
+      this.currentValue = checked;
+      const value = checked ? this.trueValue : this.falseValue;
+      this.$emit('input', value);
+      this.$emit('on-change', value);
+    }
+  }
+}
+</script>
